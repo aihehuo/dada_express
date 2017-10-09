a0b45c38caf54ab8de1942b96a9f2dd736acef3c
Bran Liang <lby89757@hotmail.com>
2017-10-09 10:41:36 +0800

    Project initialize

---
+++ lib/dada_express.rb
@@ -0,0 +1,5 @@
+require "dada_express/version"
+
+module DadaExpress
+  # Your code goes here...
+end
---
+++ spec/dada_express_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe DadaExpress do
+  it "has a version number" do
+    expect(DadaExpress::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
