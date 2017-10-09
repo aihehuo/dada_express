5aa3ea6c7afddc4c5ccf822160627ab287e75911
Bran Liang <lby89757@hotmail.com>
2017-10-09 11:01:27 +0800

    Add basic configuration test

--- spec/dada_express_spec.rb
+++ spec/dada_express_spec.rb
@@ -5,7 +5,16 @@ RSpec.describe DadaExpress do
     expect(DadaExpress::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has configuration for app_key and app_secret" do
+    DadaExpress.configure do |config|
+      config.app_key = '123456'
+      config.app_secret = 'secret'
+    end
+    expect(DadaExpress.configuration.app_key).to eq('123456')
+    expect(DadaExpress.configuration.app_secret).to eq('secret')
+  end
+
+  it "has default base url configuration" do
+    expect(DadaExpress.configuration.base_url).to eq('http://newopen.imdada.cn')
   end
 end
